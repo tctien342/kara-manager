d112ed939ad203834425ed4c94b4d9f34520a1ac
SaintNo <tctien342@gmail.com>
2018-04-14 14:04:11 +0700

    back up source

---
+++ KaraManager/database/migrations/2018_04_10_063850_create_staff_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->integer('phone');
+            $table->string('avatar')->nullable();
+            $table->string('email')->nullable();
+            $table->date('birth')->nullable();
+            $table->boolean('gender')->detault(0);
+            $table->date('join');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
