11fbe2686c8bba9724f1da7aceb39fe1c782309e
SaintNo <tctien342@gmail.com>
2019-02-17 16:51:25 +0000

    source: fix some misc

--- KaraManager/app/Http/Kernel.php
+++ KaraManager/app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\StartSession::class,
             'throttle:120,1',
             'bindings',
         ],
